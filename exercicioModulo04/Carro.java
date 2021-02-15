87157d5c0391ac9f1dc7768cbb131b915a2980a5
Luizesl14 <luiz.develop@gmail.com>
2021-02-14 22:37:10 -0300

    Criando Variaveis Staticas OBRIGATOIAS | OPICIONAIS

--- exercicioModulo04/Carro.java
+++ exercicioModulo04/Carro.java
@@ -2,4 +2,16 @@ package exercicioModulo04;
 
 public class Carro {
     
+
+    // Variaveis Staticas  OPICIONAIS
+    public static final String RED = "Vermelho";
+    public static final String WHITE = "Branco";
+    public static final String BLACK = "Preto";
+
+    //VAriaveis Privadas  OBRIGATÓRIO
+    private Integer numeroDePortas;
+    private Integer numeroDoChassi;
+    private String anoFabricacao;
+    private String combustivel;
+    
 }
