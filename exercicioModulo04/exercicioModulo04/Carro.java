acc357e04fcda31e354fedcf01ece44943cb5843
Luizesl14 <luiz.develop@gmail.com>
2021-02-14 23:48:04 -0300

    Instanciando Objeto Carro na Classe Start.java

--- exercicioModulo04/exercicioModulo04/Carro.java
+++ exercicioModulo04/exercicioModulo04/Carro.java
@@ -9,6 +9,10 @@ public class Carro {
     public static final String BLACK = "Preto";
 
     //Variaveis Privadas  OBRIGATÓRIO
+    private Integer quantidadePneus;
+	private Integer quantidadeCalotas;
+	private Integer quantidadeDeParafusos;
+
     private Integer numeroDePortas;
     private Integer numeroDoChassi;
     private String anoFabricacao;
@@ -16,12 +20,74 @@ public class Carro {
     
     //Construtor 
 	public Carro(Integer numeroDePortas, Integer numeroDoChassi, String anoFabricacao, String combustivel) {
-		super();
-		this.numeroDePortas = numeroDePortas;
-		this.numeroDoChassi = numeroDoChassi;
-		this.anoFabricacao = anoFabricacao;
-		this.combustivel = combustivel;
+		setNumeroDePortas(numeroDePortas);
+		setNumeroDoChassi(numeroDoChassi);
+		setAnoFabricacao(anoFabricacao);
+		setCombustivel(combustivel);
+        
+		/**
+		 * todo Carro deve te 04 RODAS mais 01 ESTEPE
+		 * por este motivo já intanciei no contrutor este valor
+		 * junto com os demais que necessitam dete valor
+		 * 
+		 * Obs. Se houver algum carro com mais de 05 rodas será possivel adicionar atraves 
+		 * do methodo Setter.
+		 */
+		setQuantidadePneus(4);
+		setQuantidadeCalotas(getQuantidadePneus());
+		setQuantidadeDeParafusos(getQuantidadePneus());
+		
 	}
+
+	/**
+	 * Segue methodos Getters e Setters  
+	 * Pneus
+	 * Calotas
+	 * Parafusos
+	 * 
+	 */
+
+	//Sett Gett Quantidade de Pneus
+	public Integer getQuantidadePneus(){
+		return quantidadePneus;
+	}
+	public Integer setQuantidadePneus(Integer quantidadePneus){
+		quantidadePneus = 5;
+		return this.quantidadePneus = quantidadePneus;
+	}
+
+	//Sett Gett Quantidade de Calotas
+	public Integer getQuantidadeCalotas(){
+		return quantidadeCalotas;
+	}
+	public Integer setQuantidadeCalotas(Integer quantidadeCalotas){
+		quantidadeCalotas = quantidadePneus;
+		return this.quantidadeCalotas = quantidadeCalotas;
+	}
+
+	//Sett Gett Quantidade de Parafusos
+	public Integer getQuantidadeDeParafusos(){
+		return this.quantidadeDeParafusos;
+	}
+	public Integer setQuantidadeDeParafusos(Integer quantidadeDeParafusos){
+		quantidadeDeParafusos = this.quantidadeCalotas * 4;
+		return this.quantidadeDeParafusos = quantidadeDeParafusos;
+	}
+
+	/**
+	 * 
+	 * ==========================================================================
+	 */
+
+	
+	/**
+	 * Segue methodos Getters e Setters  
+	 * Numero de Portas
+	 * Numero de Chassi
+	 * Ano de Fabricação
+	 * Combustão
+	 * 
+	 */
 	
 	//Sett Gett Numero de Portas
 	public Integer getNumeroDePortas() {
@@ -58,13 +124,31 @@ public class Carro {
 
 	// Sett Gett Variáveis Staticas
 	public static String getRed() {
+		System.out.println("Cor do Carro escolhida: " + RED);
 		return RED;
 	}
 	public static String getWhite() {
+		System.out.println("Cor do Carro escolhida: " + WHITE);
 		return WHITE;
 	}
 	public static String getBlack() {
+		System.out.println("Cor do Carro escolhida: " + BLACK);
 		return BLACK;
+	}
+
+	/**
+	 * 
+	 * ==========================================================================
+	 */
+
+
+	 
+
+	//Imprimir
+	public void imprimirValores(){
+		System.out.println("Qaunidade de Pneus: " + getQuantidadePneus());
+		System.out.println("Qaunidade de Calotas: " + getQuantidadeCalotas());
+		System.out.println("Qaunidade de Pneus: " + getQuantidadeDeParafusos());
 	}
     
     
--- exercicioModulo04/exercicioModulo04/Start.java
+++ exercicioModulo04/exercicioModulo04/Start.java
@@ -2,7 +2,20 @@ package exercicioModulo04;
 
 public class Start {
      public static void main(String[] args) {
-         
+
+        /**
+         * Instanciando Objeto Carro
+         * Integer Numero de Protas, Integer Numero de Chassi, String Ano de Fabricação, String Combustivel
+         */
+
+        Carro carro  = new Carro( 4,  123456,  "01/02/2020",  "Gasolina"); // itens obrigatórios
+
+        // Escolhendo cor do Carro
+        System.out.println("Cor do Carro: " + Carro.BLACK);
+        
+        // Imprimindo Valores
+        carro.imprimirValores();
+
          
      }
 }
