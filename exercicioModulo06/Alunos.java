9ca17b9a4d30be4e41a4f30bc6a4a4bddff59a3e
Luizesl14 <luiz.develop@gmail.com>
2021-02-17 22:19:58 -0300

    Criação classe Alunos.java e classe Start.java

---
+++ exercicioModulo06/Alunos.java
@@ -0,0 +1,18 @@
+package exercicioModulo06;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Alunos {
+    
+    // atributos
+    ArrayList<String> alunos  = new ArrayList<>();
+
+    // Datas
+    List<String> datas = new ArrayList<>();
+
+    // matriz
+    String[][] tabela =  new String[6][6];
+
+
+}
