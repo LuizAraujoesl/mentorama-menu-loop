1b34a4e366adc73435a92c6ae34d0e76f6fa5791
Luizesl14 <luiz.develop@gmail.com>
2021-02-20 22:04:46 -0300

    Criando Construtor e methodo toString

---
+++ exercicioModulo07/Conta.java
@@ -0,0 +1,72 @@
+package exercicioModulo07;
+
+public class Conta {
+
+    // atributos
+    private int agencia;
+    private int numero;
+    private String banco;
+    private double saldo;
+
+    // costrutor
+    public Conta(int agencia, int numero, String banco, double saldo){
+
+          this.agencia = agencia;
+          this.numero = numero;
+          this.banco = banco;
+          this.saldo = saldo;
+    }
+
+    // Getter and Setter Agencia
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+
+    }
+
+    // Getter and Setter Numero
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+
+    }
+
+    // Getter and Setter Banco
+    public String getBanco() {
+        return this.banco;
+    }
+
+    public void setBanco(String banco) {
+        this.banco = banco;
+
+    }
+
+    // Getter and Setter Numero
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+
+    }
+
+    @Override
+    public String toString() {
+
+        return "Conta{" +
+        "Numero= " + numero +
+        ", Agência= " + agencia +
+        ", banco= " + banco + 
+        ", Saldo= " + saldo +
+        "}";
+    }
+}
